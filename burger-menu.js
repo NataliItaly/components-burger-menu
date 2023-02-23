42e9c7163eb0e39cf08b06c937db09252d1c5e8d
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-02-24 00:13:08 +0100

    solution

---
+++ burger-menu.js
@@ -0,0 +1,31 @@
+const menu = document.getElementById('burger-menu');
+const menuOpen = document.getElementById('burger-menu-open');
+const headerLinks = document.querySelectorAll('.header__link');
+const burgerCheckbox = document.getElementById('burger-checkbox');
+const BODY = document.body;
+
+if (menuOpen) {
+    menuOpen.addEventListener('click', function() {
+        if (burgerCheckbox.checked) {
+            menu.removeAttribute('data-open', '');
+            BODY.classList.remove('hidden');
+        }
+        else {
+            menu.setAttribute('data-open', '');
+            BODY.classList.add('hidden');
+        }
+    });
+
+    headerLinks.forEach((link) => {
+      link.addEventListener('click', function (event) {
+        menu.removeAttribute('data-open');
+        burgerCheckbox.checked = false;
+        BODY.classList.remove('hidden');
+      });
+    });
+}
+
+
+
+
+
